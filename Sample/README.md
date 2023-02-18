5ed0dc608be6702b3fdae8e652bc7c2015d25a03
reets <117296082+reets93@users.noreply.github.com>
2023-02-18 01:30:20 +0000

    added content for readme file. content needs to be style correctly (template literals).

---
+++ Sample/README.md
@@ -0,0 +1,30 @@
+# sample
+            ENTER LICENSE HERE
+            ## Description
+            it's a sample project
+
+            ## Table of Contents
+            ENTER LINKABLE THINGS HERE
+
+            ## Installation
+            To install necessary depencies, run the following command:
+            npm i
+
+            ## Usage
+            You can use this application by running "  "
+
+            ## License
+            This project is under the MIT license.
+
+            ## Contributing 
+            they can't
+
+            ## Tests 
+            To run tests, run the following command:
+            npm test
+
+            ## Questions
+            If you have any questions about the repo, open an issue or contact directly on rc@gmail.com.
+            To see more of my work, go to: 
+            [reets93](https://github.com/reets93/)
+            
--- index.js
+++ index.js
@@ -86,12 +86,12 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile() {
-    const fileContent = `# ${data.title}`
+// function writeToFile() {
+//     const fileContent = `# ${data.title}`
 
-    fs.writeFile('sample.md', fileContent, (err) =>
-        err ? console.error(err) : console.log('Success'))
-}
+//     fs.writeFile('sample.md', fileContent, (err) =>
+//         err ? console.error(err) : console.log('Success'))
+// }
 
 
 
@@ -101,12 +101,55 @@ function init() {
     inquirer
         .prompt(questions).then((data) => {
             console.log('answered')
-            const fileContent = `# ${data.title}`
+            const fileContent = `# ${data.title}
+            ENTER LICENSE HERE
+            ## Description
+            ${data.description}
 
-            fs.writeFile('sample.md', fileContent, (err) =>
+            ## Table of Contents
+            ENTER LINKABLE THINGS HERE
+
+            ## Installation
+            To install necessary depencies, run the following command:
+            ${data.installation}
+
+            ## Usage
+            You can use this application by running " ${data.usage} "
+
+            ## License
+            This project is under the ${data.license} license.
+
+            ## Contributing 
+            ${data.contributing}
+
+            ## Tests 
+            To run tests, run the following command:
+            ${data.tests}
+
+            ## Questions
+            If you have any questions about the repo, open an issue or contact directly on ${data.email}.
+            To see more of my work, go to: 
+            [${data.username}](https://github.com/${data.username}/)
+            `
+
+            fs.writeFile('./Sample/README.md', fileContent, (err) =>
                 err ? console.error(err) : console.log('Success'))
         })
 }
 
 // function call to initialize program
 init();
+
+
+// switch case for badge
+// function badge() { // --> how to get badge data in the right place
+//     const licenseType = ${data.license}
+//     switch (licenseType) {
+//         case "MIT":
+//             " [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+//             break;
+
+//             'Apache License 2.0', 'GNU General Publice License v3.0', 'BSD 2-Clause "Simplified" License', 'Boost Software License', 'The Unlicense', 'Mozilla Public Licese 2.0']
+
+//     }
+// }
